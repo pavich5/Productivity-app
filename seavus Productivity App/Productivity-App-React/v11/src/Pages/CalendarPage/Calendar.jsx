e8a862a4806b970f338ad38c7d132901d7d34356
Filip Tomashevski <93556752+FilipTomash@users.noreply.github.com>
2023-07-06 16:54:50 +0200

    update

--- seavus Productivity App/Productivity-App-React/v11/src/Pages/CalendarPage/Calendar.jsx
+++ seavus Productivity App/Productivity-App-React/v11/src/Pages/CalendarPage/Calendar.jsx
@@ -1,3 +1,78 @@
+// import React, { useEffect, useState } from "react";
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+// import "./Calendar.css";
+// import Aside from "../../Layouts/Aside/Aside";
+// import Button from "../../Components/Button/Button";
+
+// const DatePickingPage = () => {
+//   const [selectedDate, setSelectedDate] = useState(new Date().toDateString());
+//   const [newTask, setNewTask] = useState("");
+//   const [dateTasks, setDateTasks] = useState({});
+
+//   useEffect(() => {
+//     console.log(dateTasks[selectedDate]);
+//   }, [selectedDate]);
+
+//   const handleDateChange = (date) => {
+//     setSelectedDate(date.toDateString());
+//   };
+
+//   const handleAddTask = () => {
+//     if (newTask.trim() !== "") {
+//       setDateTasks((prevData) => {
+//         const prevTasks = prevData[selectedDate] ? prevData[selectedDate] : [];
+
+//         const newTasks = [...prevTasks, newTask];
+
+//         return { ...prevData, [selectedDate]: newTasks };
+//       });
+//       setNewTask("");
+//     }
+//   };
+
+//   const handleDeleteTask = (index) => {
+//     setDateTasks((prevData) => {
+//       const updatedTasks = [...prevData[selectedDate]];
+//       updatedTasks.splice(index, 1);
+//       return { ...prevData, [selectedDate]: updatedTasks };
+//     });
+//   };
+
+//   return (
+//     <div className="DatePickingPageContainer">
+//       <Aside />
+//       <div className="DatePickingPage">
+//         <div className="DatePickerContainer">
+//           <DatePicker
+//             selected={new Date(selectedDate)}
+//             onChange={handleDateChange}
+//             calendarClassName="custom-calendar"
+//             dayClassName={() => "custom-day"}
+//           />
+//         </div>
+//         <h2 style={{ margin: 0 }}>Tasks for {selectedDate}</h2>
+//         <div className="AddTaskContainer">
+//           <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder="Enter a new task" />
+//           <Button onBtnClick={handleAddTask} btnText="Add Task" className="AddTaskButton" />
+//         </div>
+//         <div className="TasksContainer">
+//           <ul>
+//             {dateTasks[selectedDate]?.map((task, index) => (
+//               <li key={index}>
+//                 <span>{task}</span>
+//                 <Button onBtnClick={() => handleDeleteTask(index)} btnText="X" className="DeleteTaskButton" />
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DatePickingPage;
+
 import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -9,6 +84,7 @@ const DatePickingPage = () => {
   const [selectedDate, setSelectedDate] = useState(new Date().toDateString());
   const [newTask, setNewTask] = useState("");
   const [dateTasks, setDateTasks] = useState({});
+  const [showAlert, setShowAlert] = useState(false);
 
   useEffect(() => {
     console.log(dateTasks[selectedDate]);
@@ -19,23 +95,23 @@ const DatePickingPage = () => {
   };
 
   const handleAddTask = () => {
-    setDateTasks((prevData) => {
-      const prevTasks = prevData[selectedDate] ? prevData[selectedDate] : [];
-
-      const newTasks = [...prevTasks, newTask];
-
-      return { ...prevData, [selectedDate]: newTasks };
-    });
-
-    console.log(dateTasks);
+    if (newTask.trim() !== "" && newTask.length >= 5) {
+      setDateTasks((prevData) => {
+        const prevTasks = prevData[selectedDate] ? prevData[selectedDate] : [];
+        const newTasks = [...prevTasks, newTask];
+        return { ...prevData, [selectedDate]: newTasks };
+      });
+      setNewTask("");
+      setShowAlert(false);
+    } else {
+      setShowAlert(true);
+    }
   };
 
   const handleDeleteTask = (index) => {
-    setDateTasks((prevData) => {
-      const updatedTasks = [...prevData[selectedDate]];
-      updatedTasks.splice(index, 1);
-      return { ...prevData, [selectedDate]: updatedTasks };
-    });
+    const updatedTasks = [...dateTasks[selectedDate]];
+    updatedTasks.splice(index, 1);
+    setDateTasks((prevData) => ({ ...prevData, [selectedDate]: updatedTasks }));
   };
 
   return (
@@ -52,7 +128,14 @@ const DatePickingPage = () => {
         </div>
         <h2 style={{ margin: 0 }}>Tasks for {selectedDate}</h2>
         <div className="AddTaskContainer">
-          <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder="Enter a new task" />
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="Enter a new task"
+            className={showAlert ? "input-alert" : ""}
+          />
+          {showAlert && <div className="alert-bubble">Task must be at least 4 characters long. Please try scheduling a task again.</div>}
           <Button onBtnClick={handleAddTask} btnText="Add Task" className="AddTaskButton" />
         </div>
         <div className="TasksContainer">
